09df49ae042dbf4b008828f6ac8edc0852a33078
Lekst <spekt509@gmail.com>
2023-04-08 19:38:47 +0900

    Start

--- app/src/main/java/com/example/diplomclear/ChangePassword.java
+++ app/src/main/java/com/example/diplomclear/ChangePassword.java
@@ -3,6 +3,7 @@ package com.example.diplomclear;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class ChangePassword extends AppCompatActivity {
 
@@ -10,5 +11,9 @@ public class ChangePassword extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_change_password);
+    }
+
+    public void BackActivity(View view) {
+        this.finish();
     }
 }
--- app/src/main/java/com/example/diplomclear/Registration.java
+++ app/src/main/java/com/example/diplomclear/Registration.java
@@ -2,7 +2,9 @@ package com.example.diplomclear;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Registration extends AppCompatActivity {
 
@@ -10,5 +12,8 @@ public class Registration extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+    }
+    public void BackActivity(View view) {
+        this.finish();
     }
 }
