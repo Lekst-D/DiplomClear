f0eef42584f32714ec6a1ec9e87ca22922a263d3
Lekst <spekt509@gmail.com>
2023-05-29 16:37:16 +0900

    category search

--- app/src/main/java/com/example/diplomclear/Calendar.java
+++ app/src/main/java/com/example/diplomclear/Calendar.java
@@ -3,12 +3,40 @@ package com.example.diplomclear;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class Calendar extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private FirebaseUser user;
+    private DatabaseReference myRef;
+    private String IdUser;
+    private DatabaseReference mDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
+
+        myRef = FirebaseDatabase.getInstance().getReference();
+        mAuth = FirebaseAuth.getInstance();
+        user = mAuth.getCurrentUser();
+        IdUser = user.getUid().toString();
+
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+
+        ImageButton IDBack = findViewById(R.id.IDBack);
+        IDBack.setOnClickListener(
+                new View.OnClickListener() {
+                    public void onClick(View v) {
+                        finish();
+                    }
+                });
     }
 }
