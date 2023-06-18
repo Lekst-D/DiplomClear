b5c037f3a4178c761acaad734c1dfed427c19d78
Lekst <spekt509@gmail.com>
2023-06-18 18:09:44 +0900

    Version 1

--- app/src/main/java/com/example/diplomclear/LogRegSwap/Login.java
+++ app/src/main/java/com/example/diplomclear/LogRegSwap/Login.java
@@ -5,7 +5,9 @@ import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -91,8 +93,10 @@ public class Login extends AppCompatActivity {
 
     public void CompleteLogin(){
 
-        Intent intent = new Intent(this, Pages.class);
-        startActivity(intent);
+//        Intent intent = new Intent(this, Pages.class);
+//        startActivity(intent);
+
+        check();
 
 //        Intent intent1 = new Intent(android.provider.Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
 //        intent1.setData(Uri.parse("package:" + getPackageName()));
@@ -104,6 +108,138 @@ public class Login extends AppCompatActivity {
 //        intent1.setData(uri);
 //        startActivity(intent1);
 
+    }
+
+    public void check()
+    {
+//         проверка наличия разрешения на использование камеры
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            // разрешение не предоставлено
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.CAMERA}, MY_PERMISSIONS_REQUEST_CAMERA);
+
+        }
+        else if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.INTERNET) != PackageManager.PERMISSION_GRANTED) {
+            // разрешение не предоставлено
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.INTERNET}, MY_PERMISSIONS_REQUEST_INTERNET);
+
+        } else if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_NETWORK_STATE) != PackageManager.PERMISSION_GRANTED) {
+            // разрешение не предоставлено
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_NETWORK_STATE}, MY_PERMISSIONS_REQUEST_ACCESS_NETWORK_STATE);
+
+        }
+        else if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            // разрешение не предоставлено
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.READ_EXTERNAL_STORAGE}, MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE);
+
+        }
+//        else if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+//            // разрешение не предоставлено
+//            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.WRITE_EXTERNAL_STORAGE}, MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE);
+//
+//        }
+        else {
+            Intent intent = new Intent(this, Pages.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+//                    Intent intent = new Intent(this, AddImages.class);
+            startActivity(intent);
+        }
+    }
+
+
+    private final int MY_PERMISSIONS_REQUEST_CAMERA = 100;
+    private final int MY_PERMISSIONS_REQUEST_INTERNET = 101;
+    private final int MY_PERMISSIONS_REQUEST_ACCESS_NETWORK_STATE = 102;
+    private final int MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE = 103;
+    private final int MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE = 104;
+
+    // вызывается после ответа пользователя на запрос разрешения
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+
+            case MY_PERMISSIONS_REQUEST_CAMERA: {
+                // если пользователь закрыл запрос на разрешение, не дав ответа, массив grantResults будет пустым
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    check();
+
+                    // разрешение было предоставлено
+                    // выполните здесь необходимые операции для включения функциональности приложения, связанной с запрашиваемым разрешением
+                } else {
+                    ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.CAMERA}, MY_PERMISSIONS_REQUEST_CAMERA);
+                    // разрешение не было предоставлено
+                    // выполните здесь необходимые операции для выключения функциональности приложения, связанной с запрашиваемым разрешением
+                }
+
+            }
+
+            case MY_PERMISSIONS_REQUEST_INTERNET: {
+                // если пользователь закрыл запрос на разрешение, не дав ответа, массив grantResults будет пустым
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    check();
+
+                    // разрешение было предоставлено
+                    // выполните здесь необходимые операции для включения функциональности приложения, связанной с запрашиваемым разрешением
+                } else {
+                    ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.INTERNET}, MY_PERMISSIONS_REQUEST_INTERNET);
+                    // разрешение не было предоставлено
+                    // выполните здесь необходимые операции для выключения функциональности приложения, связанной с запрашиваемым разрешением
+                }
+
+            }
+
+            case MY_PERMISSIONS_REQUEST_ACCESS_NETWORK_STATE: {
+                // если пользователь закрыл запрос на разрешение, не дав ответа, массив grantResults будет пустым
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    check();
+
+                    // разрешение было предоставлено
+                    // выполните здесь необходимые операции для включения функциональности приложения, связанной с запрашиваемым разрешением
+                } else {
+                    ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_NETWORK_STATE}, MY_PERMISSIONS_REQUEST_ACCESS_NETWORK_STATE);
+                    // разрешение не было предоставлено
+                    // выполните здесь необходимые операции для выключения функциональности приложения, связанной с запрашиваемым разрешением
+                }
+
+            }
+
+            case MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE: {
+                // если пользователь закрыл запрос на разрешение, не дав ответа, массив grantResults будет пустым
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    check();
+
+                    // разрешение было предоставлено
+                    // выполните здесь необходимые операции для включения функциональности приложения, связанной с запрашиваемым разрешением
+                } else {
+                    ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE);
+                    // разрешение не было предоставлено
+                    // выполните здесь необходимые операции для выключения функциональности приложения, связанной с запрашиваемым разрешением
+                }
+
+            }
+
+            case MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE: {
+                // если пользователь закрыл запрос на разрешение, не дав ответа, массив grantResults будет пустым
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    check();
+
+                    // разрешение было предоставлено
+                    // выполните здесь необходимые операции для включения функциональности приложения, связанной с запрашиваемым разрешением
+                } else {
+                    ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE);
+                    // разрешение не было предоставлено
+                    // выполните здесь необходимые операции для выключения функциональности приложения, связанной с запрашиваемым разрешением
+                }
+
+
+            }
+            return;
+        }
     }
 
     public void ShowPassword(View view){
